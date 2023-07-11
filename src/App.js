b6b1399af79c4038eb5ffaa4b834f385869ed1d8
anilsagi <mail2anilsagi@gmail.com>
2023-07-11 15:13:49 +0530

    portals_first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import  ReactDOM  from 'react-dom';
+
+//using portal we can display the content outside the parent
+function Model(props){
+  return (
+    ReactDOM.createPortal(
+      <div className='model-overlay'>
+      <div className="content">
+        <h1>
+            Model Heading
+        </h1>
+        <p>
+          This is Modal content, click close to close the modal
+        </p>
+        <button className='btn btn-danger' onClick={props.close}>Close</button>
+      </div>
+  </div>
+      ,document.getElementById("model-root"))
+   
+    )
+}
 
 function App() {
+
+  const[show, setShow] = useState(false);
+
+  const toogleModel = ()=>{
+    return setShow(!show)
+;  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='btn btn-primary m-5' onClick={toogleModel}>Open modal</button>
+
+      {show && <Model close={toogleModel} />}
     </div>
   );
 }
